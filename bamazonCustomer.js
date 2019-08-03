07f5cc16c2bbc211c8d6f297d6d174201d8a3be2
Nathan Hill <pjsstp@gmail.com>
2019-08-03 08:06:38 -0400

    finished basic assignment - purchase functionality complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,7 +27,79 @@ const start = () => {
     connection.query("SELECT * FROM products", 
     function(err,res) {
         if (err) throw err
-        console.log(res)
-        connection.end()
+        displayProductList(res)
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Please enter the Id of the product you want to buy:',
+                name: 'purchaseID',
+                validate: validateNum
+            }]).then(firstResponse => {
+                if (parseInt(firstResponse.purchaseID) > 0) {
+                    inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'How many of this item do you wish to buy?',
+                        name: 'purchaseQty',
+                        validate: validateNum
+                    }
+                    ]).then(response => {
+                        buyProduct(parseInt(firstResponse.purchaseID), parseInt(response.purchaseQty))
+                    }) 
+                }
+                else connection.end()
+            })
     })
+}
+
+const displayProductList = (productArray) => {
+    console.log('+----+-------------------------+--------+')
+    console.log('| ID | PRODUCT NAME            | PRICE  |')
+    console.log('+----+-------------------------+--------+')
+    productArray.map(element => {
+        let outputString = '|'
+            if (element.item_id < 10) outputString += ` ${element.item_id}  | `
+            else outputString += ` ${element.item_id} | `
+        outputString += (element.product_name)
+        for (let i = outputString.length; i < 31; i++){
+            outputString += ' '
+        }
+        outputString += "| "
+        outputString += `$${element.price}`
+        for (let i = outputString.length; i < 40; i++){
+            outputString += ' '
+        }
+        outputString += '|'
+        console.log(outputString)
+        console.log('+----+-------------------------+--------+')
+        
+    })
+    console.log('TYPE ID="0" TO EXIT')
+}
+
+const validateNum = (input) => {
+    var reg = /^\d+$/;
+    return reg.test(input) || "Input must be a number."
+}
+
+const buyProduct = (prod_ID, quantity) => {
+    connection.query("SELECT * FROM products WHERE ?", 
+        {item_ID: prod_ID}, function(err, record) {
+            if (err) throw err
+            if (record[0].quantity >= quantity) {
+                // update the record to reflect the new quantity
+                let newQty = record[0].quantity - quantity
+                let purchCost = quantity * record[0].price
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{quantity: newQty},{item_ID: prod_ID}],
+                     function(err,purchRes) {
+                        if (err) throw err
+                        console.log(`Your total for this purchase will be $${purchCost}.00.`)
+                     })
+            }
+            else {
+                console.log("Sorry, there is not enough inventory to fill that order.")
+            }
+        start()
+        })
 }
